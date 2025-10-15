7959bdc275b32c534eb90e8b56f7fd366d41734f
Megjafari <megberserker@gmail.com>
2025-10-15 15:18:28 +0200

    Added Transaction class

---
+++ Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudgetTracker
+{
+    public class Transaction
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+
+        public Transaction(string description, decimal amount, DateTime date, string category)
+        {
+            Description = description;
+            Amount = amount;
+            Date = date;
+            Category = category;
+        }
+        public void Showinfo()
+        {
+            Console.ForegroundColor = Amount < 0 ? ConsoleColor.Red : ConsoleColor.Green;
+            Console.WriteLine($"{Date.ToShortDateString()} - {Description}: {Amount:C} [{Category}]");
+            Console.ResetColor();
+        }
+    }
+}
